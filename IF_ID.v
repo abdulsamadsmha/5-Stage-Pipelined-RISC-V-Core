d03a83db5a74f15d754c86374e793cf1539cb086
ABDUL SAMAD <abdulsamadsmha@gmail.com>
2026-07-17 02:04:15 +0530

    Fix formatting and add Valid_D output in IF_ID module

--- IF_ID.v
+++ IF_ID.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 module IF_ID(input StallD,input FlushD,input clk,input [31:0]PCF,
-input [31:0]PCPlus4F,
+input [31:0]PCPlus4F,input Valid_F,
 input[31:0] InstrF,output
-reg [31:0]InstrD,output reg[31:0]PCD,output reg[31:0] PCPlus4D,input rst
+reg [31:0]InstrD,output reg[31:0]PCD,output reg[31:0] PCPlus4D,input rst,output reg Valid_D
 
     );
     always@(posedge clk )begin
@@ -10,16 +10,20 @@ reg [31:0]InstrD,output reg[31:0]PCD,output reg[31:0] PCPlus4D,input rst
     PCD <= 32'b0;
     PCPlus4D <= 32'b0;
     InstrD <= 32'h00000013;
+    Valid_D <= 1'b0;
     end
     else if (FlushD)begin
     PCD <= 32'b0;
     PCPlus4D <= 32'b0;
     InstrD <= 32'h00000013;
+    Valid_D <= 1'b0;
     end
     else if (~StallD)begin
     PCD <= PCF;
     PCPlus4D <= PCPlus4F;
     InstrD<=InstrF;
+    Valid_D <= Valid_F;
     end
     end
 endmodule
+
