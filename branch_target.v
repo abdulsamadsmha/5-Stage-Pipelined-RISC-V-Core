9e74bde66ff5f43eb1b5d4bad6679c7ec7dab05c
ABDUL SAMAD <abdulsamadsmha@gmail.com>
2026-07-17 02:02:25 +0530

    Modify PCTargetE assignment with conditional logic

--- branch_target.v
+++ branch_target.v
@@ -3,7 +3,11 @@
 module branch_target (
     input  [31:0] PCE,
     input  [31:0] ImmExtE,
-    output [31:0] PCTargetE
+    output [31:0] PCTargetE,
+    input [31:0]ALUResultE,
+    input Branch_Target_selE
 );
-    assign PCTargetE = PCE + ImmExtE;
+    
+    assign PCTargetE = (Branch_Target_selE) ? (PCE + ImmExtE) : (ALUResultE & ~32'h1);
 endmodule
+
