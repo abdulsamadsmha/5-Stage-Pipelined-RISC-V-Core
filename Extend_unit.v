0a5d3c4e5f4c2b15d5306cdb145ca0b905159db3
ABDUL SAMAD <abdulsamadsmha@gmail.com>
2026-07-17 02:05:11 +0530

    Change ImmSrcD from 2-bit to 3-bit in Extend module

--- Extend_unit.v
+++ Extend_unit.v
@@ -1,14 +1,16 @@
 `timescale 1ns / 1ps
-module Extend(input [1:0]ImmSrcD,input[31:7] InstrD,output reg[31:0] ImmExtD
+module Extend(input [2:0]ImmSrcD,input[31:7] InstrD,output reg[31:0] ImmExtD
 
     );
     always @(*)begin
     case (ImmSrcD)
-    2'b00:ImmExtD = {{20{InstrD[31]}}, InstrD[31:20]};
-    2'b01:ImmExtD ={{20{InstrD[31]}}, InstrD[31:25], InstrD[11:7]};
-    2'b10:ImmExtD ={{20{InstrD[31]}}, InstrD[7], InstrD[30:25], InstrD[11:8], 1'b0};
-    2'b11:ImmExtD = {{12{InstrD[31]}}, InstrD[19:12], InstrD[20], InstrD[30:21], 1'b0};
+    3'b000:ImmExtD = {{20{InstrD[31]}}, InstrD[31:20]};
+    3'b001:ImmExtD ={{20{InstrD[31]}}, InstrD[31:25], InstrD[11:7]};
+    3'b010:ImmExtD ={{20{InstrD[31]}}, InstrD[7], InstrD[30:25], InstrD[11:8], 1'b0};
+    3'b011:ImmExtD = {{12{InstrD[31]}}, InstrD[19:12], InstrD[20], InstrD[30:21], 1'b0};
+    3'b100:ImmExtD = {InstrD[31:12],12'b0};
     default: ImmExtD = 32'b0;  
     endcase
     end
 endmodule
+
