9d29e3944a3547c44634df2148de60b59ecae670
ABDUL SAMAD <abdulsamadsmha@gmail.com>
2026-07-17 01:59:33 +0530

    Update pc_logic.v

--- pc_logic.v
+++ pc_logic.v
@@ -1,16 +1,22 @@
 `timescale 1ns / 1ps
 module pc_logic(input clk,input StallF,input PCSrcE,
 output reg [31:0]PCF,input[31:0] PCTargetE
-,input rst 
+,input rst ,input mret_redirect,input [31:0] mepc,input trap_redirect,
+input [31:0] mtvec
     );
     always @(posedge clk)begin
     if (rst )begin
-    PCF <= 32'b0;
-    end else if(~StallF)begin
-    case(PCSrcE)
-    1'b0:PCF <= PCF+4;
-    1'b1:PCF <= PCTargetE;
-    endcase
+    PCF <= 32'h80000000;
+    end else if (trap_redirect)
+    PCF <= mtvec;
+    else if (mret_redirect)
+    PCF <= mepc;
+    else if(~StallF)begin
+    if (PCSrcE)
+    PCF <= PCTargetE;
+    else
+    PCF <= PCF+4;
     end
     end
 endmodule
+
