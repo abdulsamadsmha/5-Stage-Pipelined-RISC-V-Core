d38b8b481c324044f10fe8a1e7db5b474c5b57cb
ABDUL SAMAD <abdulsamadsmha@gmail.com>
2026-07-17 02:05:39 +0530

    Change ALUControlD width from 3 to 4 bits

--- ALUDecoder.v
+++ ALUDecoder.v
@@ -1,24 +1,33 @@
 `timescale 1ns / 1ps
 module ALUDecoder(input [2:0]funct3,input funct7_5,input op_5,input [1:0]ALUOp,
-output reg [2:0]ALUControlD
+output reg [3:0]ALUControlD
     );
     always @(*)begin
     case(ALUOp)
-    2'b00:ALUControlD = 3'b000;
-    2'b01:ALUControlD = 3'b001;
+    2'b00:ALUControlD = 4'b0000;
+    2'b01:ALUControlD = 4'b0001;
     2'b10:begin
     case(funct3)
     3'b000:begin if(op_5&funct7_5) 
-    ALUControlD = 3'b001;
+    ALUControlD = 4'b0001;
     else
-    ALUControlD = 3'b000;
+    ALUControlD = 4'b0000;
     end
-    3'b111:ALUControlD = 3'b010;
-    3'b110:ALUControlD = 3'b011; 
-    default:ALUControlD = 3'b000; 
+    3'b111:ALUControlD = 4'b0010;
+    3'b110:ALUControlD = 4'b0011; 
+    3'b001:ALUControlD = 4'b0100;
+    3'b101:begin if(funct7_5) 
+    ALUControlD = 4'b0110;
+    else
+    ALUControlD = 4'b0101;
+    end
+    3'b010:ALUControlD = 4'b0111;
+    3'b011:ALUControlD = 4'b1000;
+    3'b100:ALUControlD = 4'b1001;
+    default:ALUControlD = 4'b0000; 
     endcase
     end
-    default:ALUControlD = 3'b000;
+    default:ALUControlD = 4'b0000;
     endcase
     end
 endmodule
